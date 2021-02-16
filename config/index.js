db443f219fdb2de263c5089b6664c639df54d9e6
Valentin F <valentin.fosse.76@gmail.com>
2021-02-16 17:50:01 +0100

    Bot creation

---
+++ config/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    FB: {
+        ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+        VERIFY_TOKEN: process.env.VERIFY_TOKEN,
+        APP_SECRET: process.env.APP_SECRET
+    },
+    TMDB: process.env.TMDB
+}
